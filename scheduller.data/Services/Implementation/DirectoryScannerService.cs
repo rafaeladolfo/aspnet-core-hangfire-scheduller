c3731d225c31110afc23e826202be5ecb42c34d0
Rafael Adolfo <rafadev1989@gmail.com>
2019-10-20 19:51:22 -0300

    Add service classes to scan activedirectory user accounts

---
+++ scheduller.data/Services/Implementation/DirectoryScannerService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scheduller.data.Services.Implementation
+{
+    public class DirectoryScannerService
+    {
+        public void ScanDeactivatedAccounts()
+        {
+            string path = "LDAP://dc=example,dc=local";
+            DirectoryEntry directoryRoot = new DirectoryEntry(path);
+            DirectorySearcher searcher = new DirectorySearcher(directoryRoot,
+                "(&(objectClass=User)(objectCategory=Person))");
+            SearchResultCollection results = searcher.FindAll();
+            foreach (SearchResult result in results)
+            {
+                DirectoryEntry de = result.GetDirectoryEntry();
+                if (IsActive(de))
+                    DisableDependantAccounts(de.Username);
+
+            }
+        }
+
+        private bool IsActive(DirectoryEntry de)
+        {
+            if (de.NativeGuid == null) return false;
+            int flags = (int)de.Properties["userAccountControl"].Value;
+
+            return !Convert.ToBoolean(flags & 0x0002);
+        }
+
+        private void DisableDependantAccounts(string userName)
+        {
+
+        }
+    }
+}
